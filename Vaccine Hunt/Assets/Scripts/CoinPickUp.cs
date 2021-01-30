d87a0efdb378c8885fe25e5a1427f9b658f00de0
nimeshac <nimeshac@hsenidmobile.com>
2021-01-31 00:45:13 +0530

    coin count done

---
+++ Vaccine Hunt/Assets/Scripts/CoinPickUp.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinPickUp : MonoBehaviour {
+
+void  OnTriggerEnter ( Collider col  ){
+ transform.position = new Vector3(0, -1000, 0);
+ GlobalCoins.CoinCount += 1;
+}
+}
---
+++ Vaccine Hunt/Assets/Scripts/CoinRotate.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinRotate : MonoBehaviour {
+void  Update (){
+ transform.Rotate(0, 2, 0, Space.World);
+}
+}
